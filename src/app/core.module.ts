53c2875f941af17a9cf8eb86be4c9edd1568a642
cc <1826515410@qq.com>
2020-12-24 16:17:50 +0800

    初始化样式和增加coreModule

---
+++ src/app/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ]
+})
+export class CoreModule { }
